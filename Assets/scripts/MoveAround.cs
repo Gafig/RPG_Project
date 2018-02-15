d62a7e81a7aec4737d06c06e02046985d8fe97a9
Gafig <authorgafig@gmail.com>
2018-02-15 11:34:44 +0700

    add test walk around

--- Assets/scripts/MoveAround.cs
+++ Assets/scripts/MoveAround.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class MoveAround : MonoBehaviour
 {
@@ -9,6 +10,17 @@ public class MoveAround : MonoBehaviour
     void Update()
     {
         var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+        float speed = this.speed;
+        this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, move, 0.26F);
+
+        foreach (RaycastHit hit in hits.OrderBy(x => x.distance))
+        {
+            speed = 0f;
+        }
         transform.position += move * speed * Time.deltaTime;
     }
+
 }
