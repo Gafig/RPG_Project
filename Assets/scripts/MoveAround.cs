1c16e6a9506ed9a42b7157200a3b2790486eee6c
Gafig <authorgafig@gmail.com>
2018-03-04 14:55:34 +0700

    init field of view (bugs AF)

--- Assets/scripts/MoveAround.cs
+++ Assets/scripts/MoveAround.cs
@@ -4,26 +4,38 @@ using System.Linq;
 
 public class MoveAround : MonoBehaviour
 {
-
+    Rigidbody rigidbody;
     float speed = 5.0f;
+    Vector3 velocity;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
         if (GameMasterController.IsInputEnabled)
         {
-            var move = Vector3.Normalize(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0));
-
-            float speed = this.speed;
-            this.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            RaycastHit[] hits;
-            hits = Physics.RaycastAll(transform.position, move, 0.26F);
-
-            foreach (RaycastHit hit in hits.OrderBy(x => x.distance))
-            {
-                speed = 0f;
-            }
-            transform.position += move * speed * Time.deltaTime;
+            velocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0).normalized * speed;
+            transform.LookAt(velocity);
+           
+           
         }
     }
 
+    private void FixedUpdate()
+    {
+        float speed = this.speed;
+        this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        RaycastHit[] hitsX = null, hitsY = null;
+        if(velocity.x != 0)
+            hitsX = Physics.RaycastAll(transform.position, new Vector3(velocity.x, 0, 0), 0.26F);
+        if(velocity.y != 0)
+            hitsY = Physics.RaycastAll(transform.position, new Vector3(0, velocity.y, 0), 0.26F);
+        if (hitsX == null || hitsX.Length == 0)
+            rigidbody.MovePosition(rigidbody.position + new Vector3(velocity.x, 0, 0) * Time.fixedDeltaTime);
+        if (hitsY == null || hitsY.Length == 0)
+            rigidbody.MovePosition(rigidbody.position + new Vector3(0, velocity.y, 0) * Time.fixedDeltaTime);
+    }
 }
