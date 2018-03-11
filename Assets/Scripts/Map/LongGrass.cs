23db08c1bc619b8cc5d37f54690bd5a8fea68544
lunaticSKE12 <jamesnapongd@gmail.com>
2018-03-11 15:13:18 +0700

    Random appear Monster and change to combat sence

---
+++ Assets/Scripts/Map/LongGrass.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LongGrass : MonoBehaviour {
+
+	public BiomeList grassType;
+
+	private GameManager gm;
+
+	// Use this for initialization
+	void Start () {
+		gm = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.GetComponent<PlayerMovement> ()) {
+			
+			//prob = x / 187.5
+			float vc = 10 / 187.5f;
+			float c = 8.5f / 187.5f;
+			float sr = 6.75f / 187.5f;
+			float r = 3.33f / 187.5f;
+			float vr = 1.25f / 187.5f;
+
+			float p = Random.Range (0.0f, 100.0f);
+
+			Debug.Log ("p"+p);
+			Debug.Log ("vc"+vc);
+			Debug.Log ("c"+c);
+			Debug.Log ("sr"+sr);
+			Debug.Log ("r"+r);
+			Debug.Log ("vr"+vr);
+
+
+			if (p > 98) {
+				if (gm != null)
+					gm.EnterBattle (Rarity.VeryRare);
+			} 
+			else if (p > 93) {
+				if (gm != null)
+					gm.EnterBattle (Rarity.Rare);
+			}
+			else if (p > 85) {
+				if (gm != null)
+					gm.EnterBattle (Rarity.SemiRare);
+			}
+			else if (p > 70) {
+				if (gm != null)
+					gm.EnterBattle (Rarity.Common);
+			}
+			else {
+				if (gm != null)
+					gm.EnterBattle (Rarity.VeryCommon);
+			}
+
+		}
+	}
+}
+
---
+++ Assets/Scripts/Monster/BasePokemon.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BasePokemon : MonoBehaviour {
+
+	public string PName;
+	public Sprite image;
+	public BiomeList biomeFound;
+	public PokemonType type;
+	public Rarity rarity;
+	public int HP;
+	private int maxHP;
+	public Stat AttackStat;
+	public Stat DefenceStat;
+
+	public PokemonStats pokemonStats;
+
+	public bool canEvolve;
+	public PokemonEvolution evolveTo;
+
+	private int level;
+
+	// Use this for initialization
+	void Start () {
+		maxHP = HP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Addmember(BasePokemon bp){
+		this.PName = bp.PName;
+		this.image = bp.image;
+		this.biomeFound = bp.biomeFound;
+		this.type = bp.type;
+		this.rarity = bp.rarity;
+		this.HP = bp.HP;
+		this.maxHP = bp.maxHP;
+		this.AttackStat = bp.AttackStat;
+		this.DefenceStat = bp.DefenceStat;
+		this.pokemonStats = bp.pokemonStats;
+		this.canEvolve = bp.canEvolve;
+		this.evolveTo = bp.evolveTo;
+		this.level = bp.level;
+	}
+}
+
+public enum Rarity{
+	VeryCommon,
+	Common,
+	SemiRare,
+	Rare,
+	VeryRare
+}
+
+
+public enum PokemonType{
+	Flying,
+	Ground,
+	Rock,
+	Steel,
+	Fire,
+	Water,
+	Grass,
+	Ice,
+	Electric,
+	Phychic,
+	Dark,
+	Dragon,
+	Fighting,
+	Normal
+
+}
+
+[System.Serializable]
+
+public class PokemonEvolution{
+	public BasePokemon newEvolution;
+	public int levelUpLevel;
+
+}
+
+[System.Serializable]
+
+public class PokemonStats{
+	public int AttackStat;
+	public int DefenceStat;
+	public int SpeedStat;
+	public int SpAttackStat;
+	public int SpDefenceStat;
+	public int EvasionStat;
+}
