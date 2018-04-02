9a0668cdfd6f3512adff37da017d0ffabd2ca080
Gafig <authorgafig@gmail.com>
2018-04-02 17:54:05 +0700

    clean folder

---
+++ Assets/scripts/Interaction/InteractableObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour, Interactable {
+
+    public new string name;
+    public Event interaction;
+
+    public void react()
+    {
+        throw new NotImplementedException();
+    }
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
