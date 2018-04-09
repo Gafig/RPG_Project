078d1e8a514ffa56133379215b9ae3c7905b790b
Gafig <authorgafig@gmail.com>
2018-04-09 12:07:09 +0700

    add DateTime field

---
+++ Assets/Scripts/Controller/DateTimeController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DateTimeController : MonoBehaviour {
+
+    public static DateTimeController instance;
+
+    private const int MINUTES_IN_A_DAY = 1440;
+
+    public int timeInMinutes;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        timeInMinutes = 0;
+        StartCoroutine(testUpdateTime());
+	}
+
+    IEnumerator testUpdateTime()
+    {
+        while (true) {
+            yield return new WaitForSeconds(1.0f);
+            timeInMinutes += 60;
+            Debug.Log("time: " + timeInMinutes);
+        }
+    }
+
+    public void updateTimeInHour(int hour)
+    {
+        updateTime(60 * hour);
+    }
+
+    public void updateTime(int minute)
+    {
+        timeInMinutes += minute;
+    }
+
+    public int getDate()
+    {
+        return (timeInMinutes / MINUTES_IN_A_DAY) + 1;
+    }
+
+    public void setTimeTo(int day, int hour, int minute)
+    {
+        timeInMinutes = day * MINUTES_IN_A_DAY + hour * 60 + minute;
+    }
+
+    public void setTimeTo(int day, string time)
+    {
+        timeInMinutes = day * MINUTES_IN_A_DAY;
+        if (time.Equals("Midnight"))
+            timeInMinutes += 0;
+        else if (time.Equals("Dawn"))
+            timeInMinutes += 6 * 60;
+        else if (time.Equals("Morning"))
+            timeInMinutes += 8 * 60;
+        else if (time.Equals("Afternoon"))
+            timeInMinutes += 12 * 60;
+        else if (time.Equals("Evening"))
+            timeInMinutes += 16 * 60;
+        else if (time.Equals("Night"))
+            timeInMinutes += 18 * 60;
+    }
+
+    public string getTime()
+    {
+        int currentTime = getcurrentTimeInDay();
+        if (currentTime < 6)
+            return "Night";
+        else if (currentTime < 8)
+            return "Dawn";
+        else if (currentTime < 12)
+            return "Morning";
+        else if (currentTime < 16)
+            return "Afternoon";
+        else if (currentTime < 18)
+            return "Evening";
+        else if (currentTime < 24)
+            return "Night";
+        else
+            return "Unknown Time and Space";
+    }
+
+    public int getcurrentTimeInDay()
+    {
+        return timeInMinutes % MINUTES_IN_A_DAY / 60;
+    }
+}
---
+++ Assets/Scripts/DateTime/DateTimePanelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DateTimePanelManager : MonoBehaviour {
+
+    [SerializeField]
+    TextMeshProUGUI date;
+    [SerializeField]
+    TextMeshProUGUI time;
+
+	// Update is called once per frame
+	void Update () {
+        string datetext = string.Format("Day: {0:d2}", DateTimeController.instance.getDate());
+        date.SetText( datetext );
+        time.SetText(DateTimeController.instance.getTime());		
+	}
+}
