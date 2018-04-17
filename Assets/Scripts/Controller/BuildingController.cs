4ad6c67516d2c5fb86c7485566cf61c5d4f90cce
Gafig <authorgafig@gmail.com>
2018-04-17 19:30:09 +0700

    add house 1

---
+++ Assets/Scripts/Controller/BuildingController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingController : MonoBehaviour {
+
+    void Start()
+    {
+        GameMasterController.instance.setPlayerToTheLastDoor();
+    }
+
+}
