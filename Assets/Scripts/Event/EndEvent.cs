1a36e0ecd0c94a02054ec8ed22d43c54c916d488
Gafig <authorgafig@gmail.com>
2018-04-24 14:32:33 +0700

    add prefabs

---
+++ Assets/Scripts/Event/EndEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndEvent : Event {
+
+    public override void trigger()
+    {
+        GameMasterController.instance.endEvent();
+    }
+}
---
+++ Assets/Scripts/Event/StartEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartEvent : Event {
+
+    public override void trigger()
+    {
+        GameMasterController.instance.startEvent();
+        triggerNextEvent();
+    }
+}
