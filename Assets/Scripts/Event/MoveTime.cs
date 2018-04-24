c57f72acc1409f864cddefa81eb626eb4d8344f1
Gafig <authorgafig@gmail.com>
2018-04-24 16:51:03 +0700

    fix

---
+++ Assets/Scripts/Event/MoveTime.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTime : Event {
+
+    [SerializeField]
+    int timeToAdd;
+
+    public override void trigger()
+    {
+        DateTimeController.instance.updateTime(timeToAdd);
+        triggerNextEvent();
+    }
+}
