739777e2b2ad749de71312166b1fccc6129c9e42
Gafig <authorgafig@gmail.com>
2018-04-30 19:04:13 +0700

    setCameras

---
+++ Assets/Scripts/SetDefaultCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetDefaultCamera : MonoBehaviour {
+
+    [SerializeField]
+    Camera defautCamera;
+    public Camera[] cameras;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("set Cam");
+        cameras = GameObject.FindObjectsOfType<Camera>();
+        foreach (Camera c in cameras)
+            c.enabled = false;
+        defautCamera.enabled = true;
+	}
+}
