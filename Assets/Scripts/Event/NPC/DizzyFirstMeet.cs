7a0474d24fe9aacd40b7d37cd5ffa2ef3c75526d
Gafig <authorgafig@gmail.com>
2018-05-01 18:18:26 +0700

    add introduction

---
+++ Assets/Scripts/Event/NPC/DizzyFirstMeet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DizzyFirstMeet : Event {
+
+    public override void trigger()
+    {
+        StartCoroutine(tryStart());
+    }
+
+    IEnumerator tryStart()
+    {
+        yield return new WaitUntil(() => GameMasterController.instance.startEvent());
+        triggerNextEvent();
+    }
+
+    private void Update()
+    {
+        if (FlagController.instance.hospitalVisit == 0)
+        {
+            FlagController.instance.hospitalVisit++;
+            trigger();
+        }
+    }
+}
