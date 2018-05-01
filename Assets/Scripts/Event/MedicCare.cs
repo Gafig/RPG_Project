f08afb7915e252d85616f059de9b960eea45196b
Gafig <authorgafig@gmail.com>
2018-05-01 18:55:22 +0700

    add dun introduction

--- Assets/Scripts/Event/MedicCare.cs
+++ Assets/Scripts/Event/MedicCare.cs
@@ -7,6 +7,9 @@ public class MedicCare : Event {
     public override void trigger()
     {
         Debug.Log("Medic care");
+        if (Party.instance != null)
+            foreach (Character c in Party.instance.partyList)
+                c.fullHeal();
         triggerNextEvent();
     }
 }
--- Assets/Scripts/Event/Sleep.cs
+++ Assets/Scripts/Event/Sleep.cs
@@ -7,6 +7,9 @@ public class Sleep : Event {
     public override void trigger()
     {
         Debug.Log("Sleep zzzz");
+        if (Party.instance != null)
+            foreach (Character c in Party.instance.partyList)
+                c.heal((int)(c.maxHP * 0.2f));
         triggerNextEvent();
     }
 }
